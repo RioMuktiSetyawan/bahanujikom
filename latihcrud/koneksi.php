10be9e032dfc7655b48cb09abba8c2f77fafb475
RioMuktiSetyawan <riomukti583@gmail.com>
2025-04-27 17:06:27 +0700

    Add files via upload

---
+++ latihcrud/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+$server = "localhost";
+$user   = "root";
+$password = "";
+$database = "latihancrud";
+
+$koneksi = mysqli_connect($server, $user, $password, $database) or die(myslqi_error($koneksi));
+
+
+
+
+?>
