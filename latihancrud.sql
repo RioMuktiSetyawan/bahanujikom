4d74eb2113c76e6c432f2b257bc52808066a5595
RioMuktiSetyawan <riomukti583@gmail.com>
2025-04-28 18:45:42 +0700

    Add files via upload

--- latihancrud.sql
+++ latihancrud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2025 at 12:04 PM
+-- Generation Time: Apr 28, 2025 at 01:45 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -25,6 +25,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `login_admin`
+--
+
+CREATE TABLE `login_admin` (
+  `id` int(10) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login_admin`
+--
+
+INSERT INTO `login_admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', '123456');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tmhs`
 --
 
@@ -41,11 +60,18 @@ CREATE TABLE `tmhs` (
 --
 
 INSERT INTO `tmhs` (`id_mhs`, `nim`, `nama`, `alamat`, `prodi`) VALUES
-(1, '221011400649', 'Bagus Supriyanto', 'Jl. Tanggerang', 'S1 - Teknik Informatika');
+(1, '221011400647', 'Bagus Supriyanto', 'Jl. Tanggerang', 'S1 - Teknik Informatika'),
+(2, '221011400648', 'Rio Mukti Setyawan', 'Jl. Krukut Depok', 'S1 - Managemen');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `login_admin`
+--
+ALTER TABLE `login_admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tmhs`
@@ -58,10 +84,16 @@ ALTER TABLE `tmhs`
 --
 
 --
+-- AUTO_INCREMENT for table `login_admin`
+--
+ALTER TABLE `login_admin`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `tmhs`
 --
 ALTER TABLE `tmhs`
-  MODIFY `id_mhs` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_mhs` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
